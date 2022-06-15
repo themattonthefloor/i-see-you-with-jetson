0ffaae62b6d72bb0819621f82ae1f106e1b90d63
zi_wei_ooi <zi_wei_ooi@aiap.sg>
2022-06-16 00:50:28 +0800

    Add tegra_cam.py

--- src/cam_loop.py
+++ src/cam_loop.py
@@ -1,7 +1,16 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+def open_cam_onboard(width, height):
+    # On versions of L4T previous to L4T 28.1, flip-method=2
+    # Use Jetson onboard camera
+    gst_str = ("nvcamerasrc ! "
+               "video/x-raw(memory:NVMM), width=(int)2592, height=(int)1458, format=(string)I420, framerate=(fraction)30/1 ! "
+               "nvvidconv ! video/x-raw, width=(int){}, height=(int){}, format=(string)BGRx ! "
+               "videoconvert ! appsink").format(width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+cap = open_cam_onboard(1920,1080)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
 while True:
