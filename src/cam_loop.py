c0bea1a3aeb876f8a99cd28f2a510777152339bc
zi_wei_ooi <zi_wei_ooi@aiap.sg>
2022-06-16 00:31:40 +0800

    Add cam_loop.py

---
+++ src/cam_loop.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+while True:
+    ret, img = cap.read()
+    # img = cv2.flip(img, -1)
+    cv2.imshow('video',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
