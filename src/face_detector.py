7bc65e0b9caae44b0d767e3021ff5c0f4cd2a68e
zi_wei_ooi <zi_wei_ooi@aiap.sg>
2022-07-14 02:05:05 +0800

    Add create_face_img function

--- src/face_detector.py
+++ src/face_detector.py
@@ -8,7 +8,7 @@ from time import time
 
 def parse_args():
     """
-    Parse input arguments
+    Input arguments parser.
     """
     parser = argparse.ArgumentParser(description="Capture and display live camera video on Jetson TX2/TX1")
     parser.add_argument("--usb", dest="use_usb",
@@ -16,6 +16,9 @@ def parse_args():
                         action="store_true")
     parser.add_argument("--cpu_only", dest="cpu_only",
                         help="use CPU only",
+                        action="store_true")
+    parser.add_argument("--verbose", dest="verbose",
+                        help="print detection outputs",
                         action="store_true")
     args = parser.parse_args()
     return args
@@ -46,6 +49,55 @@ def gstreamer_pipeline(
         )
     )
 
+def furnish_image(frame, face, confidence):
+    """Draws bounding boxes & prints detection confidence.
+
+    Args:
+        frame (3D-Numpy Array): cv2 image
+        face (list): list of detected faces
+        confidence (list): list of face detection confidence
+
+    Returns:.
+    
+        3D-Numpy Array: cv2 image
+    """
+    # loop through detected faces
+    for idx, f in enumerate(face):
+        
+        (startX, startY) = f[0], f[1]
+        (endX, endY) = f[2], f[3]
+
+        # draw rectangle over face
+        cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
+
+        text = "{:.2f}%".format(confidence[idx] * 100)
+
+        Y = startY - 10 if startY - 10 > 10 else startY + 10
+
+        # write confidence percentage on top of face rectangle
+        cv2.putText(frame, text, (startX,Y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                    (0,255,0), 2)
+        
+    return frame
+
+############
+def create_face_images(frame, face, new_size=(120,180)):
+
+    # loop through detected faces
+    face_imgs = []
+    for idx, f in enumerate(face):
+        
+        (startX, startY) = f[0], f[1]
+        (endX, endY) = f[2], f[3]
+
+        # crop the faces
+        face_img = frame[startY:endY,startX:endX]
+        face_img = cv2.resize(face_img,new_size)
+        face_imgs.append(face_img)
+        
+    return face_imgs
+###########
+
 def live_inference(args):
 
     window_title = "i-see-you-with-jetson"
@@ -62,30 +114,19 @@ def live_inference(args):
 
             # apply face detection
             face, confidence = cv.detect_face(frame, threshold=0.5, enable_gpu = not args.cpu_only)
-            
-            print(f"{time()-elapsed_time:.2f} S")
+            presence = len(face)>0
+
+            if args.verbose:
+                print(f"Frame time: {time()-elapsed_time:.3f}s | Face: {face} | Confidences: {confidence}")
+
+            # Reset elapsed time    
             elapsed_time = time()
-            print(face)
-            print(confidence)
 
-            # loop through detected faces
-            for idx, f in enumerate(face):
-                
-                (startX, startY) = f[0], f[1]
-                (endX, endY) = f[2], f[3]
+            if presence:
+                # Furnish image
+                frame = furnish_image(frame, face, confidence)
 
-                # draw rectangle over face
-                cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
-
-                text = "{:.2f}%".format(confidence[idx] * 100)
-
-                Y = startY - 10 if startY - 10 > 10 else startY + 10
-
-                # write confidence percentage on top of face rectangle
-                cv2.putText(frame, text, (startX,Y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-                            (0,255,0), 2)
-
-            # display output
+            # Display output
             cv2.imshow(window_title, frame)
 
             k = cv2.waitKey(30) & 0xff
@@ -98,3 +139,6 @@ def live_inference(args):
 if __name__ == '__main__':
     args = parse_args()
     live_inference(args=args)
+
+# windows: python -m src.face_detector --usb --cpu
+# jetson: python -m src.face_detector
