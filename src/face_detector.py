aca730aacfa2fb0e062660cb9b3e17d3e62d9d3f
zi_wei_ooi <zi_wei_ooi@aiap.sg>
2022-07-27 05:09:32 +0800

    WIP: src/face_detector.py

--- src/face_detector.py
+++ src/face_detector.py
@@ -23,8 +23,10 @@ def parse_args():
     parser.add_argument("--verbose", dest="verbose",
                         help="print detection outputs",
                         action="store_true")
-    parser.add_argument("--tolerance", dest="tolerance", default=0.6, type=float,
+    parser.add_argument("--match_tolerance", dest="tolerance", default=0.6, type=float,
                         help="How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.")
+    parser.add_argument("--img_dir", dest="img_dir", default="data", type=str,
+                        help="directory to save facial images")
     args = parser.parse_args()
     return args
 
@@ -54,7 +56,7 @@ def gstreamer_pipeline(
         )
     )
 
-def furnish_image_confidence(frame, faces, confidences):
+def furnish_image_confidence(frame, faces, confidences, names):
     """Draws bounding boxes & prints detection confidence.
 
     Args:
@@ -71,14 +73,12 @@ def furnish_image_confidence(frame, faces, confidences):
         (startX, startY) = face[0], face[1]
         (endX, endY) = face[2], face[3]
 
-        # draw rectangle over face
+        # Draw rectangle over face
         cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
 
-        text = "{:.2f}%".format(confidences[idx] * 100)
-
+        # Print name and confidence
+        text = f"{names[idx]} ({(confidences[idx] * 100):.2f})" #"{:.2f}%".format(confidences[idx] * 100)
         Y = startY - 10 if startY - 10 > 10 else startY + 10
-
-        # write confidence percentage on top of face rectangle
         cv2.putText(frame, text, (startX,Y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
                     (0,255,0), 2)
         
@@ -96,35 +96,46 @@ def create_face_images(frame, faces, new_size=(120,180)):
     Returns:
         list: List of 3D-Numpy arrays cv2 image of faces
     """
-    # loop through detected faces
+    # Loop through detected faces
     face_imgs = []
     for idx, face in enumerate(faces):
         
         (startX, startY) = face[0], face[1]
         (endX, endY) = face[2], face[3]
 
-        # crop the faces
+        # Crop the faces
         face_img = frame[startY:endY,startX:endX]
         face_img = cv2.resize(face_img,new_size)
         face_imgs.append(face_img)
         
     return face_imgs
 
-def init_known_faces(dir="../data/", filename="faces.pkl"):
-    if filename not in os.listdir(dir):
-        df = pd.DataFrame(columns=["ID","name","encoding","confidence"])
+def init_known_faces(dir="data", pkl_filename="faces.pkl"):
+    if pkl_filename not in os.listdir(dir):
+        df = pd.DataFrame(columns=["name","encoding","confidence"])
     else:
-        df = pd.read_pickle(os.path.join(dir,filename))
+        df = pd.read_pickle(os.path.join(dir,pkl_filename))
+        names_dict = [x.replace(".jpg","").split("_") for x in os.listdir(dir) if x.endswith(".jpg")]
+        names_dict = {int(x[0]): (x[1] if len(x)>1 else "Unknown") for x in names_dict}
+        print(names_dict)
+        df["name"] = df.index.map(names_dict)
     return df
 
-def save_known_faces(df, dir="../data/", filename="faces.pkl"):
+def save_known_faces(df, dir="data", filename="faces.pkl"):
     df.to_pickle(os.path.join(dir,filename))
 
-def update_known_faces(df, name, enc, confidence, dir="../data/", filename="faces.pkl"):
-    ID = 0 if len(df)==0 else df["ID"].max()+1
-    new_row = {"ID":ID,"name":name,"encoding":enc,"confidence":confidence}
+def update_known_faces(df, name, enc, confidence, dir="data", filename="faces.pkl"):
+    new_row = {"name":name,"encoding":enc,"confidence":confidence}
     new_df = df.append(new_row, ignore_index=True)
     return new_df
+
+def get_details(df, idx):
+    details = df.iloc[idx]
+    if details['name']=="Unknown":
+        name = "#" + str(idx)
+    else:
+        name = details['name']
+    return name, details['encoding'], details['confidence']
 
 def name_to_faces(faces):
     pass
@@ -143,6 +154,7 @@ def live_inference(args):
         test_enc = np.array([])
         # INITIATE KNOWN ENCODINGS
         known_faces_df = init_known_faces()
+        print(known_faces_df)
 
         while True:
 
@@ -156,25 +168,34 @@ def live_inference(args):
             presence = len(faces)>0
 
             if presence:
-                encs = face_encodings(frame,faces) # List of arrays
-                # if len(test_enc)==0:
-                #     test_enc = encs[0]
-                # print(compare_faces([test_enc],encs[0]),confidences)
+                encs = face_encodings(frame,faces)
                 names = []
                 for i, enc in enumerate(encs):
                     comparison = compare_faces(known_faces_df["encoding"].to_list(), enc)
-                    if max(comparison):
-                        names.append(known_faces_df["names"][])
+                    if len(comparison)>0 and max(comparison):
+                        name, _, _ = get_details(known_faces_df, np.argmax(comparison))
+                        names.append(name)
+                    else: # Update name, encoding, confidence, facial image file
+                        name = "Unknown"
+                        names.append(name)
+                        confidence = confidences[i]
+                        known_faces_df = update_known_faces(known_faces_df, name, enc, confidence)
+
+                        ID_str = str(known_faces_df.index.max()).zfill(4)
+                        face_img = create_face_images(frame, [faces[i]])[0]
+                        face_img_save_path = os.path.join(args.img_dir,ID_str)+".jpg"
+                        cv2.imwrite(face_img_save_path, face_img)
+
 
                 # Furnish image
-                frame = furnish_image_confidence(frame, faces, confidences)
-
+                frame = furnish_image_confidence(frame, faces, confidences, names)
 
             # Display output
             cv2.imshow(window_title, frame)
 
             if args.verbose:
-                print(f"Frame time: {time()-elapsed_time:.3f}s | Face: {faces} | Confidences: {confidences}")
+                print(f"Frame time: {time()-frame_start_time:.3f}s | Face: {faces} | Confidences: {confidences}")
+
 
             k = cv2.waitKey(30) & 0xff
             if k == 27: # press 'ESC' to quit
@@ -182,6 +203,7 @@ def live_inference(args):
     finally:
         cap.release()
         cv2.destroyAllWindows()
+        save_known_faces(known_faces_df)
 
 if __name__ == '__main__':
     args = parse_args()
